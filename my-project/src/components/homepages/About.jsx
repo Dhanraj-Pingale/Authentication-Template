e0a1b67bc3129b1e380504f9d3e1071426428328
YashBarbole <yashbarbole7110@gmail.com>
2024-09-26 22:10:07 +0530

    yash test

--- my-project/src/components/homepages/About.jsx
+++ my-project/src/components/homepages/About.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>This is the about section</div>
+    <div>This is the about section
+      testing yash
+
+    </div>
   )
 }
 
